236fd8db5140f90b9ae07e04360b903953b715e2
Guilherme <silver@RLS-Legacy.home>
2025-09-25 18:52:20 +0100

    yaaaaaaa

---
+++ src/clev_plot.js
@@ -0,0 +1,56 @@
+const margin = {top: 30, right: 30, left:200, bottom: 30};
+
+const width = 1000 - margin.left - margin.right;
+const height = 460 - margin.top - margin.bottom;
+
+const svg = d3.select("#clev_dot")
+    .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+var dataCSV = d3.csv("../dataset/crocodile_dataset_processed.csv");
+dataCSV.then(function(data) {
+    const x = d3.scaleLinear()
+        .domain([0,1200])
+        .range([0, width]);
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x))
+
+    const y = d3.scaleBand()
+        .range([0, height])
+        .domain(data.map(function(d) { return d.commonname; })).padding(1);
+    svg.append("g")
+        .call(d3.axisLeft(y))
+
+    svg.selectAll("lines")
+        .data(data)
+        .enter()
+        .append("line")
+            .attr("x1", function(d) { return x(d.length); })
+            .attr("x2", function(d) { return x(d.weight); })
+            .attr("y1", function(d) { return y(d.commonname); })
+            .attr("y2", function(d) { return y(d.commonname); })
+            .attr("stroke", "grey")
+            .attr("stroke-width", "1px")
+
+    svg.selectAll("circles")
+        .data(data)
+        .enter()
+        .append("circle")
+            .attr("cx", function(d) { return x(d.length); })
+            .attr("cy", function(d) { return y(d.commonname); })
+            .attr("r", "6")
+            .style("fill", "#69b3a2")
+
+    svg.selectAll("circles")
+        .data(data)
+        .enter()
+        .append("circle")
+            .attr("cx", function(d) { return x(d.weight); })
+            .attr("cy", function(d) { return y(d.commonname); })
+            .attr("r", "6")
+            .style("fill", "#4C4082")
+});
